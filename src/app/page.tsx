78fdd8e05b79bc141d81a5114e315f1163697bc4
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2023-11-17 00:44:50 +0900

    fix: resolve issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function IndexPage() {
   return (
     <main className="flex flex-col justify-center items-center gap-8 p-12">
-      <h1 className="text-2xl]">Stylish Next App</h1>
+      <h1 className="text-2xl">Stylish Next App</h1>
     </main>
   );
 }
