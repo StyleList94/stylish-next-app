a62647a2c2d660fd3bbb13491cc754cae16ca542
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2025-08-05 16:56:15 +0900

    refactor: lint-staged
    * chore: patch Next.js

---
+++ lint-staged.config.mjs
@@ -0,0 +1,13 @@
+import path from 'node:path';
+import process from 'node:process';
+
+/**
+ * @filename: lint-staged.config.mjs
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  '*.{js,jsx,ts,tsx}': (filenames) => {
+    const files = filenames.map((file) => path.relative(process.cwd(), file));
+    return `next lint --fix ${files.map((file) => `--file ${file}`).join(' ')}`;
+  },
+};
